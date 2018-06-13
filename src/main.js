a67b8c6de626f3d7a25b1ef36ae49747e18ee3c9
pedja77 <pedja77@stcable.net>
2018-06-13 19:00:35 +0200

    Initial commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue"
+import App from "./App.vue"
+
+import "bootstrap/dist/css/bootstrap.css"
+import "bootstrap/dist/js/bootstrap.js"
+
+import router from "./routes/router"
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app")
