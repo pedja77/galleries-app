1b665ea7748c562a491e687e13bf9c1e764f38b6
pedja77 <pedja77@stcable.net>
2018-06-16 10:42:17 +0200

    CreateGallery form

--- src/pages/CreateGallery.vue
+++ src/pages/CreateGallery.vue
@@ -14,17 +14,26 @@
           <textarea id="textarea" name="textarea" cols="40" rows="5" class="form-control"></textarea>
         </div>
       </div>
-      <div class="form-group row">
+      <div class="form-group row" v-for="(url, index) in urls" :key="index">
         <label for="text1" class="col-4 col-form-label">Picture URL:</label>
-        <div class="col-8">
-          <input id="text1" name="text1" type="text" class="form-control here">
+        <div class="col-6">
+          <input id="text1" name="text1" type="text" class="form-control here" v-model="urls[index]">
+        </div>
+        <div class="col-2">
+          <button name="submit" type="button" class="btn btn-primary" :class="{disabled: index === 0}" @click="moveUp(index)">^</button>
+          <button name="submit" type="button" class="btn btn-primary" :class="{disabled: index === urls.length - 1}" @click="moveDown(index)">v</button>
+          <button name="submit" type="button" class="btn btn-danger" :class="{disabled: onlyOneUrl}" @click="removeUrl(index)">X</button>
         </div>
       </div>
       <div class="form-group row">
+        <div class="offset-4 col-8 text-right">
+          <button name="submit" type="Button" class="btn btn-primary" @click="addUrl">Add URL</button>
+        </div>
+      </div>
+
+      <div class="form-group row">
         <div class="offset-4 col-8">
           <button name="submit" type="submit" class="btn btn-primary">Submit</button>
-        </div>
-        <div class="offset-4 col-8">
           <button name="submit" type="button" class="btn btn-warning">Cancel</button>
         </div>
       </div>
@@ -35,6 +44,45 @@
 
 <script>
 export default {
+  data() {
+    return {
+      urls: ['']
+    }
+  },
+  computed: {
+    onlyOneUrl() {
+      return this.urls.length === 1
+    }
+  },
+  methods: {
+    addUrl() {
+      this.urls.push('')
+    },
+    removeUrl(index) {
+      if (this.urls.length > 1) {
+        this.urls.splice(index, 1)
+      }     
+    },
+    moveUp(index) {
+      if (index > 0) {
+        let newUrls = this.urls.slice(0)
+        let temp = newUrls[index]
+        newUrls[index] = newUrls[index - 1]
+        newUrls[index - 1] = temp
+        this.urls = newUrls
+      }
+      console.log('up', this.urls)
+    },
+    moveDown(index) {
+      if (index < this.urls.length - 1) {
+        let newUrls = this.urls.slice(0)
+        let temp = newUrls[index]
+        newUrls[index] = newUrls[index + 1]
+        newUrls[index + 1] = temp
+        this.urls = newUrls
+      }
+    }
+  }
 
 }
 </script>
